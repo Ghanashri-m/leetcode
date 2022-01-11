c4681fe3dca4c5779264c3f6b0aa7f12b883b4ec
Ghanashri M <ghanashri.28@gmail.com>
2022-01-10 22:45:10 -0800

    Time: 105 ms (15.47%), Space: 38.3 MB (89.43%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.js
+++ 266-palindrome-permutation/266-palindrome-permutation.js
@@ -2,39 +2,16 @@
  * @param {string} s
  * @return {boolean}
  */
-// var canPermutePalindrome = function(s) {
-//     const map = new Map();
-//     let oddLettersCount = 0;
-//     for (let i = 0; i < s.length; i++) {
-//         map.set(s[i], map.get(s[i]) + 1 || 1);
-//     }
-//     for (let item of map) {
-//         if (item[1] % 2) {
-//             oddLettersCount++;
-//         }
-//     }
-//     return oddLettersCount <= 1;
-// };
-
-const canPermutePalindrome = function (s) {
-
-  let map = new Map()
-// 1
-  for (let i = 0; i < s.length; i++) {
-    const ltr = s[i];
-// 2    
-    if (map.has(ltr)) {
-      map.set(ltr, map.get(ltr) + 1)
-    } else {
-      map.set(ltr, 1)
+var canPermutePalindrome = function(s) {
+    const map = new Map();
+    let oddLettersCount = 0;
+    for (let i = 0; i < s.length; i++) {
+        map.set(s[i], map.get(s[i]) + 1 || 1);
     }
-  }
-
-  let oddNumOfLtrs = 0
-// 3
-  for (ltr of map) {
-    if (ltr[1] % 2 !== 0) oddNumOfLtrs++
-  }
-// 4
-  return oddNumOfLtrs <= 1
+    for (let item of map) {
+        if (item[1] % 2) {
+            oddLettersCount++;
+        }
+    }
+    return oddLettersCount <= 1;
 };
