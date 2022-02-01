3a3f5c30ae1f98ee5cfdf5dd203d89b6836b0dfa
Ghanashri M <ghanashri.28@gmail.com>
2022-01-31 23:39:57 -0800

    Time: 128 ms (97.27%), Space: 52.5 MB (56.31%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -3,5 +3,38 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    return nums.sort((a,b) => a - b)
+    // return nums.sort((a,b) => a - b)
+    mergeSort(nums,new Array(nums.length), 0, nums.length - 1);
+    return nums;
 };
+
+var mergeSort = function(nums, temp, l, r) { // l = left, r = right
+    if (l === r) return;
+    const m = Math.floor((l + r) / 2);
+    mergeSort(nums, temp, l, m);
+    mergeSort(nums, temp, m+1, r);
+    merge(nums, temp, l, m, m + 1,  r);
+}
+
+var merge = function(nums, temp, l, m, mm,  r) {
+    let i = l, tempi = i
+    while (l <= m && mm <= r) {
+        if (nums[l] <= nums[mm]) {
+            temp[i++] = nums[l++]
+        } else {
+            temp[i++] = nums[mm++]
+        }
+    }
+    
+    while (l <= m) {
+        temp[i++] = nums[l++]
+    }
+    
+    while (mm <= r) {
+        temp[i++] = nums[mm++]
+    }
+        
+    for (let j = tempi; j <= r; j++) {
+        nums[j] = temp[j]
+    }
+}
