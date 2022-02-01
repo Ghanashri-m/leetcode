7967fa33487aca0c96de97a94a31fc42aa1cc22d
Ghanashri M <ghanashri.28@gmail.com>
2022-01-31 23:08:08 -0800

    Time: 89 ms (51.12%), Space: 44.7 MB (6.56%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -22,27 +22,27 @@ var search = function(nums, target) {
     if (target < nums[0] || target > nums[nums.length - 1]) {
         return -1;
     }
-    // let start = 0, end = nums.length - 1, mid;
-    // while (start <= end) {
-    //     mid = Math.floor((start + end) / 2);
-    //     if (nums[mid] === target) {
-    //         return mid;
-    //     } else if (nums[mid] > target) {
-    //         end = mid - 1;
-    //     } else {
-    //         start = mid + 1;
-    //     }
-    // }
-    // return -1;
-    
-    let recursiveFunction = function (arr, x, start, end) {
-        if (start > end) return -1;
-        let mid=Math.floor((start + end)/2);
-        if (arr[mid]===x) return mid;
-        if(arr[mid] > x)
-            return recursiveFunction(arr, x, start, mid-1);
-        else
-            return recursiveFunction(arr, x, mid+1, end);
+    let start = 0, end = nums.length - 1, mid;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
     }
-    return recursiveFunction(nums, target, 0, nums.length - 1);
+    return -1;
+    
+    // let recursiveFunction = function (arr, x, start, end) {
+    //     if (start > end) return -1;
+    //     let mid=Math.floor((start + end)/2);
+    //     if (arr[mid]===x) return mid;
+    //     if(arr[mid] > x)
+    //         return recursiveFunction(arr, x, start, mid-1);
+    //     else
+    //         return recursiveFunction(arr, x, mid+1, end);
+    // }
+    // return recursiveFunction(nums, target, 0, nums.length - 1);
 };
