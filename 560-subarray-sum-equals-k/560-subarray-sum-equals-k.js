04fb256317758eb0adb98473a2e6cb4bb8c9b52d
Ghanashri M <ghanashri.28@gmail.com>
2022-02-01 23:26:38 -0800

    Time: 118 ms (66.71%), Space: 49.8 MB (29.32%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
@@ -19,3 +19,8 @@ var subarraySum = function(nums, k) {
     }
     return cnt;
 };
+
+// logic, currentsum - k = value_in_map
+// currentsum = k + value_in_map
+
+// so if value_in_map is present, then k must hv occurred
