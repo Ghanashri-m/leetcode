f832cfa821d782e7515df7a5c9d33cf9744ad34e
Ghanashri M <ghanashri.28@gmail.com>
2022-02-06 19:45:04 -0800

    Time: 128 ms (48.88%), Space: 53.9 MB (5.11%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+
+    if(!headA || !headB) return null;
+    
+    let first = headA;
+    let second = headB;
+    console.log("first", first);
+    console.log("second", second)
+    while(first !== second) {
+        if(!first){
+            first = headB;
+        } else{
+            first = first.next
+        }
+        
+        if(!second){
+            second = headA;
+        } else {
+            second = second.next;
+        }   
+    }
+    return first;
+    
+};
