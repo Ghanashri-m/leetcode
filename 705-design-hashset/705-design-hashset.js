cca95ba475241be7fe84b3d59a1ff7fcd5784ba9
Ghanashri M <ghanashri.28@gmail.com>
2022-05-25 15:10:51 -0700

    Time: 267 ms (42.70%), Space: 80.2 MB (10.85%) - LeetHub

--- 705-design-hashset/705-design-hashset.js
+++ 705-design-hashset/705-design-hashset.js
@@ -1,16 +1,36 @@
 
 var MyHashSet = function() {
-    this.hashSet = {}
+    this.hashSet = new Array(1000).fill('null')
+    this.bucket = 1000;
+    this.bucketItems = 1000; // In this example square root of 10 to the power 6 is 1000, it might change based on data given
 };
+
+MyHashSet.prototype.hash1 = function(key) {
+    // To find the primary array index
+    return key % this.bucket;
+}
+
+MyHashSet.prototype.hash2 = function(key) {
+    // To find the secondary array index (offset)
+    return Math.floor(key / this.bucketItems);
+}
 
 /** 
  * @param {number} key
  * @return {void}
  */
 MyHashSet.prototype.add = function(key) {
-    if (!this.hashSet[key]) {
-      this.hashSet[key] = true;
+    let bucket = this.hash1(key);
+    let bucketItem = this.hash2(key);
+    if (this.hashSet[bucket] === 'null') {
+        if (bucket === 0) {
+            this.hashSet[bucket]= new Array(this.bucketItems + 1).fill(false);
+        } else {
+            this.hashSet[bucket] = new Array(this.bucketItems).fill(false);
+        }
     }
+    
+    this.hashSet[bucket][bucketItem]=true;
 };
 
 /** 
@@ -18,9 +38,11 @@ MyHashSet.prototype.add = function(key) {
  * @return {void}
  */
 MyHashSet.prototype.remove = function(key) {
-
-    if (this.hashSet[key]) {
-      delete this.hashSet[key];
+    let bucket = this.hash1(key);
+    let bucketItem = this.hash2(key);
+    
+    if (this.hashSet[bucket] !== 'null') {
+        this.hashSet[bucket][bucketItem] = false;
     }
 };
 
@@ -29,17 +51,8 @@ MyHashSet.prototype.remove = function(key) {
  * @return {boolean}
  */
 MyHashSet.prototype.contains = function(key) {
-    if (this.hashSet[key]) { 
-      return true; 
-    } else {
-      return false;
-    }
+    let bucket = this.hash1(key);
+    let bucketItem = this.hash2(key);
+    return this.hashSet[bucket] !== 'null' && this.hashSet[bucket][bucketItem];
 };
 
-/** 
- * Your MyHashSet object will be instantiated and called as such:
- * var obj = new MyHashSet()
- * obj.add(key)
- * obj.remove(key)
- * var param_3 = obj.contains(key)
- */
