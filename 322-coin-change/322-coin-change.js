719a92d8b0d927f557831d59ad6239c9fef1e6bd
Ghanashri M <ghanashri.28@gmail.com>
2022-06-01 17:15:55 -0700

    Time: 109 ms (92.38%), Space: 46 MB (77.48%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    const table = new Array(amount + 1).fill(Infinity);
-    table[0] = 0;
-    
-    for (let coin of coins) {
-        for (let i = 0; i < table.length; i++) {
-            if (coin <= i) {
-                let idx = i - coin;
-                potential_amt = table[idx] + 1;
-                table[i] = Math.min(potential_amt, table[i]);
-            }
-        }
+    const table = new Array(amount + 1)
+    for (let i=0; i<table.length; i++) {
+        table[i] = Infinity;
     }
-    return table[table.length - 1] === Infinity ? -1 : table[table.length - 1];
+    table[0] = 0;
+
+    coins.forEach(coin => {
+        for (let i=coin; i< table.length; i++) {
+            table[i] = Math.min(table[i], (1 + table[i-coin]));
+        }
+    });
+
+    return table[table.length-1] !== Infinity ? table[table.length-1] : -1;
 };
