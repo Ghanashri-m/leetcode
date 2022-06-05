4838ddd37a439b00d7c060916d613f586ea01e1b
Ghanashri M <ghanashri.28@gmail.com>
2022-06-04 19:21:14 -0700

    Time: 73 ms (87.72%), Space: 44.2 MB (78.36%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,29 +3,35 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let low = 0;
-    let ans = 0;
-    let high = nums.length;
+//     let low = 0;
+//     let ans = 0;
+//     let high = nums.length;
     
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2)
-        let count = 0
-        for (let i = 0; i < nums.length; ++i) {
-            if (nums[i] <= mid) {
-                count++
-            }
-        }
-        if (count > mid) {
-            /** mid is just a normal number, we should go higher */
-            low = mid + 1
-        } else {
-            /** maybe mid is the missing number but we not sure, so we should keep
-             * find the smallest possible one with (count <= mid) since
-             * only in that condition, we might able to confidently call out 
-             * mid is a missing number */
-            ans = mid
-            high = mid - 1
-        }
+//     while (low <= high) {
+//         let mid = Math.floor((low + high) / 2)
+//         let count = 0
+//         for (let i = 0; i < nums.length; ++i) {
+//             if (nums[i] <= mid) {
+//                 count++
+//             }
+//         }
+//         if (count > mid) {
+//             /** mid is just a normal number, we should go higher */
+//             low = mid + 1
+//         } else {
+//             /** maybe mid is the missing number but we not sure, so we should keep
+//              * find the smallest possible one with (count <= mid) since
+//              * only in that condition, we might able to confidently call out 
+//              * mid is a missing number */
+//             ans = mid
+//             high = mid - 1
+//         }
+//     }
+//     return ans
+    
+    let a = 0
+    for(let i = 0; i < nums.length; i++){
+        a += nums[i]
     }
-    return ans
+    return nums.length *(nums.length + 1)/2-a
 };
