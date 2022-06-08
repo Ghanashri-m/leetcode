32550ccab6c59c1eee8195036529744ca4dd8b28
Ghanashri M <ghanashri.28@gmail.com>
2022-06-07 18:06:35 -0700

    Time: 140 ms (45.68%), Space: 48 MB (85.22%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var findDiagonalOrder = function(mat) {
+	const m = mat.length;
+	const n = mat[0].length;
+	const result = [];
+
+	for (let index = 0; index < m + n - 1; index++) {
+		const top = Math.max(0, index - n + 1);
+		const bottom = Math.min(index, m - 1);
+
+		if (index % 2 === 0) {
+			for (let pos = bottom; pos >= top; pos--) {
+				result.push(mat[pos][index - pos]);
+			}
+		} else {
+			for (let pos = top; pos <= bottom; pos++) {
+				result.push(mat[pos][index - pos]);
+			}
+		}
+	}
+	return result;
+};
