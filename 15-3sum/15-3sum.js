adea7e47b3c3ff48937ce778af59b12dcc51a94b
Ghanashri M <ghanashri.28@gmail.com>
2022-06-08 19:53:11 -0700

    Time: 159 ms (79.58%), Space: 51.9 MB (93.38%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -2,30 +2,44 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// In this approach, the first thing we need to do is to sort the given array in ascending order.
+// After sorting the array, we are going to iterate through it and set our two pointers. 
+// A left pointer will be set to a number that comes immediately after the current number and a right pointer will be set to the number at the end of the array.
+// Then we are going to find our current sum which is the sum of our current number, a left number, and a right number.
+// Now we check if our current sum is equal to our target sum, which in this case is 0.
+
+// Now we check if our current sum is equal to our target sum, which in this case is 0.
+// If it is equal, we just add those three numbers to our final array (triplets).
+// If the current sum is less than 0, we move the left pointer to the right by one to increase the sum.
+// Because we earlier sorted the given array in ascending order, we know that each number is greater than the number to its left.
+
+// If the current sum is greater than 0, because we know that each number is smaller than the number to its right, we can move the right pointer to the left by one to decrease the sum.
+
 var threeSum = function(nums) {
-    let indices = [];
     nums.sort((a,b) => a - b);
+    const triplets = [];
     
     for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] !== nums[i-1]) {
+        if(nums[i] != nums[i-1]){ // making sure our solution set does not contain duplicate triplets
             let left = i + 1;
             let right = nums.length - 1;
             while (left < right) {
-                let sum = nums[i] + nums[left] + nums[right];
-                if (sum === 0) {
-                    indices.push([nums[i], nums[left], nums[right]]);
-                    while(left < right && nums[left] === nums[left + 1]) left++;
-                    while(left < right && nums[right] === nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else if (sum > 0) {
-                    right--;
+                const currentSum = nums[i] + nums[left] + nums[right];
+                if (currentSum === 0){
+                    triplets.push([nums[i], nums[left], nums[right]]);
+                    while(nums[left] == nums[left + 1]) left ++
+                    while(nums[right] == nums[right - 1]) right -- // making sure our solution set does not contain duplicate triplets
+                    left ++;
+                    right --;
+                } else if(currentSum < 0) {
+                    left ++
+                } else if(currentSum > 0){
+                    right --
                 }
             }
         }
     }
     
-    return indices;
+    return triplets;
 };
