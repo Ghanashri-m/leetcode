991b769996a708b5aa8fee063700b56e99c2a5ce
Ghanashri M <ghanashri.28@gmail.com>
2023-03-09 14:53:12 -0800

    Time: 68 ms (44.36%), Space: 43.1 MB (8.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,18 +7,45 @@ var searchRange = function(nums, target) {
     let first = -1;
     let last = -1;
     
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            first = i;
-            break;
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left+right) / 2);
+        if (nums[mid] === target) {
+            if (mid === 0 || nums[mid - 1] !== target) {
+                first = mid;
+                break;
+            }
+            right = mid - 1;
+        } else {
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
     }
     
-    for (i = nums.length - 1; i >= 0; i--) {
-        if (nums[i] === target) {
-            last = i;
-            break;
+    left = 0;
+    right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left+right) / 2);
+        if (nums[mid] === target) {
+            if (mid === (nums.length - 1) || nums[mid + 1] !== target) {
+                last = mid;
+                break;
+            }
+            left = mid + 1;
+        } else {
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
     }
+    
     return [first, last];
 };
