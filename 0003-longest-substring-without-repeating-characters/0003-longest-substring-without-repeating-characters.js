205720db2b60d7faf6969e59bb2434f57d09e108
Ghanashri M <ghanashri.28@gmail.com>
2023-03-09 13:51:23 -0800

    Time: 93 ms (65.44%), Space: 48.6 MB (35.73%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length <= 1) {
+        return s.length;
+    }
+    
+    let maxLength = 0;    
+    let currString = {}
+    let right = 0;
+    let left = 0;
+    
+    while (right < s.length && left < s.length) {
+        if (s[right] in currString) {
+            left = Math.max(left, currString[s[right]] + 1);
+            currString[s[right]] = right;
+        }
+        currString[s[right]] = right;
+        maxLength = Math.max(maxLength, (right - left + 1));
+        right++;
+    }
+
+    return maxLength;
+};
