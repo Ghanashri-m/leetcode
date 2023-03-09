1b76215037fa39028fd1650f92b2164e49859adf
Ghanashri M <ghanashri.28@gmail.com>
2023-03-09 10:20:59 -0800

    Time: 160 ms (83.87%), Space: 49.9 MB (35.48%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.js
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -8,14 +8,7 @@ var numRescueBoats = function(people, limit) {
     let lightest = 0;
     let heaviest = people.length - 1;
     let boats = 0;
-    for (let i in people) {
-        if (lightest === heaviest) {
-            boats++;
-            break;
-        }
-        if (lightest > heaviest) {
-            break;
-        }
+    while (lightest <= heaviest) {
         if (people[lightest] + people[heaviest] <= limit) {
             boats++;
             lightest++;
